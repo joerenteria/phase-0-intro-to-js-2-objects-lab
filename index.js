932f445d56b42ec4e4ff9b98a703ee41840195d0
Joe Renteria <joe@electronflight.com>
2021-07-06 17:53:51 -0700

    object lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee={
+    name: `Joe`,
+    streetAddress: `9030 Whispering Pines`,
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    return Object.assign({},obj,{[key]:value})
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    return Object.assign(obj,{[key]:value})
+}
+
+function deleteFromEmployeeByKey(obj,key,value){
+
+    delete obj.key;
+    return Object.assign({},obj,{[key]:value})
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key,value){
+
+    delete obj.key;
+    return Object.assign(obj,{[key]:value})
+    
+}
+
